7ec933a52c3d1d6ddc793f126a3a7b0f53232dc7
Jaem <91131509+DarkChocoJaem@users.noreply.github.com>
2024-08-08 07:27:24 +0900

    login screen

--- lib/src/features/auth/presentation/login/login_screen.dart
+++ lib/src/features/auth/presentation/login/login_screen.dart
@@ -4,17 +4,62 @@ import 'package:flutter/material.dart';
 
 @RoutePage()
 class LoginScreen extends StatelessWidget {
-
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return OrbScaffold(
-      appBar: OrbAppBar(
-        title: "Login",
+      appBar: OrbAppBar(),
+      body: Column(
+        children: [
+          const SizedBox(height: 32),
+          Image.asset('assets/icons/anipod_logo.png'),
+          const SizedBox(height: 32),
+          OrbTextField(
+            hintText: 'Email',
+            textInputAction: TextInputAction.next,
+          ),
+          const SizedBox(height: 16),
+          OrbTextField(
+            obscureText: true,
+            hintText: 'Password',
+          ),
+          const SizedBox(height: 16),
+          OrbFilledButton(
+            onPressed: () {},
+            text: "로그인",
+          ),
+          const SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              OrbTextButton(
+                onPressed: () {},
+                text: "아이디 찾기",
+              ),
+              const SizedBox(width: 8),
+              const OrbText(
+                "|",
+              ),
+              const SizedBox(width: 8),
+              OrbTextButton(
+                onPressed: () {},
+                text: "비밀번호 찾기",
+              ),
+              const SizedBox(width: 8),
+              const OrbText(
+                "|",
+              ),
+              const SizedBox(width: 8),
+              OrbTextButton(
+                onPressed: () {},
+                text: "회원가입 찾기",
+              ),
+            ],
+          ),
+        ],
       ),
-      body: Container(),
     );
   }
 }
